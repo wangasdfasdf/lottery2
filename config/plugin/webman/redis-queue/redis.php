b0d7f46093116185ccac2a463d012f453b5b2568
li <123@qq.com>
2023-08-26 20:33:02 +0800

    init

--- config/plugin/webman/redis-queue/redis.php
+++ config/plugin/webman/redis-queue/redis.php
@@ -1,9 +1,9 @@
 <?php
 return [
     'default' => [
-        'host' => 'redis://redis:6379',
+        'host' => sprintf('redis://%s:%s', env('REDIS_HOST'), env('REDIS_PORT') ),
         'options' => [
-            'auth' => '123456',       // 密码，字符串类型，可选参数
+            'auth' => env('REDIS_PASSWORD'),       // 密码，字符串类型，可选参数
             'db' => 1,            // 数据库
             'prefix' => env('APP_NAME').':queue:',       // key 前缀
             'max_attempts'  => 5, // 消费失败后，重试次数
