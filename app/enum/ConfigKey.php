ca2615aca4bbdfca27f82f1873ea314427975042
li <123@qq.com>
2023-09-11 14:36:44 +0800

    init

--- app/enum/ConfigKey.php
+++ app/enum/ConfigKey.php
@@ -5,4 +5,8 @@ namespace app\enum;
 enum ConfigKey: string
 {
     case U2Day2 = 'u2day2';
+
+    case DEFAULT_DOMAIN  = 'default_domain';
+
+    case DOMAIN_LISTS = 'domain_lists';
 }
--- database/seeders/ConfigSeeder.php
+++ database/seeders/ConfigSeeder.php
@@ -16,5 +16,7 @@ class ConfigSeeder extends Seeder
     public function run(): void
     {
         Config::query()->firstOrCreate(['key' => ConfigKey::U2Day2], ['name' => 'U转天数', 'value' => 2]);
+        Config::query()->firstOrCreate(['key' => ConfigKey::DEFAULT_DOMAIN], ['name' => '默认域名', 'value' => 'http://tianxin.jcprint.vip/machine']);
+        Config::query()->firstOrCreate(['key' => ConfigKey::DOMAIN_LISTS], ['name' => '域名列表', 'value' => 'http://tianxin.jcprint.vip/machine,http://tanzan.jcprint.vip,http://ss.jcprint.vip']);
     }
 }
