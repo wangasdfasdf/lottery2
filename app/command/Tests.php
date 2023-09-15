9bcea93c789ae7b4c2d12abbca3e458496d889b9
li <123@qq.com>
2023-09-15 21:32:18 +0800

    init

--- app/command/Tests.php
+++ app/command/Tests.php
@@ -20,7 +20,9 @@ use app\service\LotteryPlwResultService;
 use app\service\OssService;
 use app\service\RasService;
 use Illuminate\Support\Str;
+use mysql_xdevapi\Exception;
 use support\Db;
+use support\Log;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
@@ -51,9 +53,38 @@ class Tests extends Command
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $ids = Agent::query()->pluck('id');
+        $agentId = 1;
+
+        /**
+         * @var Agent $agent
+         */
+        $agent = Agent::query()->find($agentId);
+
+        $path1 = "/home/wwwroot/linux-client-package/attach/core.dat";
+        $tag = $agent->tag;
+        file_put_contents($path1, $tag);
+        $command = 'cd /home/wwwroot/linux-client-package && node build/builder.js';
+        exec($command, $output);
+
+        Log::info('111111', $output);
+        if (!in_array('Build complete!', $output)) {
+            throw new Exception(json_encode($output), 509);
+        }
 
 
+        $object = "client/" . Str::random() . '.exe';
+        $path = '/home/wwwroot/linux-client-package/dist_electron/contest-client_setup_1.0.0_' . $tag . '.exe';
+
+        $command2 = 'rm -f ' . $path;
+        $command3 = 'rm -f ' . $path . '.blockmap';
+
+        OssService::instance()->uploadFile($object, $path);
+
+        $agent->client_url = 'https://tianxin.jcprint.vip/' . $object;
+        $agent->save();
+
+        exec($command2);
+        exec($command3);
 
         dd(1);
         return self::SUCCESS;
