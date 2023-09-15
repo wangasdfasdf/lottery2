a1053d5a6570aea71d43be4d4ffba5d18fff474d
li <123@qq.com>
2023-09-15 22:01:44 +0800

    init

--- app/command/Tests.php
+++ app/command/Tests.php
@@ -53,38 +53,43 @@ class Tests extends Command
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $agentId = 2;
 
-        /**
-         * @var Agent $agent
-         */
-        $agent = Agent::query()->find($agentId);
+        $ids = Agent::query()->pluck('id');
 
-        $path1 = "/home/wwwroot/linux-client-package/attach/core.dat";
-        $tag = $agent->tag;
-        file_put_contents($path1, $tag);
-        $command = 'cd /home/wwwroot/linux-client-package && node build/builder.js';
-        exec($command, $output);
+        foreach ($ids as $agentId){
+            /**
+             * @var Agent $agent
+             */
+            $agent = Agent::query()->find($agentId);
 
-        Log::info('111111', $output);
-        if (!in_array('Build complete!', $output)) {
-            throw new Exception(json_encode($output), 509);
+            $path1 = "/home/wwwroot/linux-client-package/attach/core.dat";
+            $tag = $agent->tag;
+            file_put_contents($path1, $tag);
+            $command = 'cd /home/wwwroot/linux-client-package && node build/builder.js';
+            exec($command, $output);
+
+            Log::info('111111', $output);
+            if (!in_array('Build complete!', $output)) {
+                throw new Exception(json_encode($output), 509);
+            }
+
+
+            $object = "client/" . Str::random() . '.exe';
+            $path = '/home/wwwroot/linux-client-package/dist_electron/contest-client_setup_1.0.0_' . $tag . '.exe';
+
+            $command2 = 'rm -f ' . $path;
+            $command3 = 'rm -f ' . $path . '.blockmap';
+
+            OssService::instance()->uploadFile($object, $path);
+
+            $agent->client_url = 'https://tianxin.jcprint.vip/' . $object;
+            $agent->save();
+
+            exec($command2);
+            exec($command3);
         }
 
 
-        $object = "client/" . Str::random() . '.exe';
-        $path = '/home/wwwroot/linux-client-package/dist_electron/contest-client_setup_1.0.0_' . $tag . '.exe';
-
-        $command2 = 'rm -f ' . $path;
-        $command3 = 'rm -f ' . $path . '.blockmap';
-
-        OssService::instance()->uploadFile($object, $path);
-
-        $agent->client_url = 'https://tianxin.jcprint.vip/' . $object;
-        $agent->save();
-
-        exec($command2);
-        exec($command3);
 
         dd(1);
         return self::SUCCESS;
