77b5d24b15e1568e74c031b975dce95db1da9413
li <123@qq.com>
2023-09-16 16:30:51 +0800

    init

--- app/model/BaseModel.php
+++ app/model/BaseModel.php
@@ -3,6 +3,7 @@
 namespace app\model;
 
 use app\model\traits\QueryFilterTrait;
+use DateTimeInterface;
 use Illuminate\Database\Eloquent\Model;
 
 class BaseModel extends Model
@@ -13,4 +14,9 @@ class BaseModel extends Model
     protected $hidden = [
         'deleted_at',
     ];
+
+    protected function serializeDate(DateTimeInterface $date): string
+    {
+        return $date->format('Y-m-d H:i:s');
+    }
 }
