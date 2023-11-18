22ffde0239c3b401b69ff4695b87b307e3de9fa7
li <123@qq.com>
2023-11-18 16:48:45 +0800

    修改比分转换

---
+++ app/command/DeleteOrder.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace app\command;
+
+use app\middleware\traits\SetSuffix;
+use app\model\Agent;
+use app\model\AgentOrder;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Output\OutputInterface;
+
+
+class DeleteOrder extends Command
+{
+    use SetSuffix;
+
+    protected static $defaultName = 'DeleteOrder';
+    protected static $defaultDescription = 'DeleteOrder';
+
+    /**
+     * @return void
+     */
+    protected function configure()
+    {
+        $this->addArgument('name', InputArgument::OPTIONAL, 'Name description');
+    }
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return int
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $agentIds = Agent::query()->pluck('id');
+
+        foreach ($agentIds as $id) {
+            $this->setSuffix($id);
+
+            AgentOrder::query()->where('created_at', '<', now()->subDays(8)->format('Y-m-d H:i:s'))->delete();
+        }
+
+        $output->writeln('Hello DeleteOrder');
+        return self::SUCCESS;
+    }
+
+}
